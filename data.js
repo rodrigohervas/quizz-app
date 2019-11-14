91c7b8ac11965415d7c22e968b8ccbe7938902ae
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2019-11-13 19:44:23 -0500

    Added data.ja with all the app data.

---
+++ data.js
@@ -0,0 +1,255 @@
+export { stateCapitals };
+
+const stateCapitals = [
+    {
+        id: 1, 
+        state: 'Alabama', 
+        capital: 'Montgomery'
+    }, 
+    {
+        id: 2, 
+        state: 'Alaska', 
+        capital: 'Juneau'
+    },
+    {
+        id: 3, 
+        state: 'Arizona', 
+        capital: 'Phoenix'
+    },
+    {
+        id: 4, 
+        state: 'Arkansas', 
+        capital: 'Little Rock'
+    },
+    {
+        id: 5, 
+        state: 'California', 
+        capital: 'Sacramento'
+    },
+    {
+        id: 6, 
+        state: 'Colorado', 
+        capital: 'Denver'
+    },
+    {
+        id: 7, 
+        state: 'Connecticut', 
+        capital: 'Hartford'
+    },
+    {
+        id: 8, 
+        state: 'Delaware', 
+        capital: 'Dover'
+    },
+    {
+        id: 9, 
+        state: 'Florida', 
+        capital: 'Tallahassee'
+    },
+    {
+        id: 10, 
+        state: 'Georgia', 
+        capital: 'Atlanta'
+    },
+    {
+        id: 11, 
+        state: 'Hawaii', 
+        capital: 'Honolulu'
+    },
+    {
+        id: 12, 
+        state: 'Idaho', 
+        capital: 'Boise'
+    },
+    {
+        id: 13, 
+        state: 'Illinois', 
+        capital: 'Springfield'
+    },
+    {
+        id: 14, 
+        state: 'Indiana', 
+        capital: 'Indianapolis'
+    },
+    {
+        id: 15, 
+        state: 'Iowa', 
+        capital: 'Des Moines'
+    },
+    {
+        id: 16, 
+        state: 'Kansas', 
+        capital: 'Topeka'
+    },
+    {
+        id: 17, 
+        state: 'Kentucky', 
+        capital: 'Frankfort'
+    },
+    {
+        id: 18, 
+        state: 'Louisiana', 
+        capital: 'Baton Rouge'
+    },
+    {
+        id: 19, 
+        state: 'Maine', 
+        capital: 'Augusta'
+    },
+    {
+        id: 20, 
+        state: 'Maryland', 
+        capital: 'Annapolis'
+    },
+    {
+        id: 21, 
+        state: 'Massachusetts', 
+        capital: 'Boston'
+    },
+    {
+        id: 22, 
+        state: 'Michigan', 
+        capital: 'Lansing'
+    },
+    {
+        id: 23, 
+        state: 'Minnesota', 
+        capital: 'Saint Paul'
+    },
+    {
+        id: 24, 
+        state: 'Mississippi', 
+        capital: 'Jackson'
+    },
+    {
+        id: 25, 
+        state: 'Missouri', 
+        capital: 'Jefferson City'
+    },
+    {
+        id: 26, 
+        state: 'Montana', 
+        capital: 'Helena'
+    },
+    {
+        id: 27, 
+        state: 'Nebraska', 
+        capital: 'Lincoln'
+    },
+    {
+        id: 28, 
+        state: 'Nevada', 
+        capital: 'Carson City'
+    },
+    {
+        id: 29, 
+        state: 'New Hampshire', 
+        capital: 'Concord'
+    },
+    {
+        id: 30, 
+        state: 'New Jersey', 
+        capital: 'Trenton'
+    },
+    {
+        id: 31, 
+        state: 'New Mexico', 
+        capital: 'Santa Fe'
+    },
+    {
+        id: 32, 
+        state: 'New York', 
+        capital: 'Albany'
+    },
+    {
+        id: 33, 
+        state: 'North Carolina', 
+        capital: 'Raleigh'
+    },
+    {
+        id: 34, 
+        state: 'North Dakota', 
+        capital: 'Bismarck'
+    },
+    {
+        id: 35, 
+        state: 'Ohio', 
+        capital: 'Columbus'
+    },
+    {
+        id: 36, 
+        state: 'Oklahoma', 
+        capital: 'Oklahoma City'
+    },
+    {
+        id: 37, 
+        state: 'Oregon', 
+        capital: 'Salem'
+    },
+    {
+        id: 38, 
+        state: 'Pennsylvania', 
+        capital: 'Harrisburg'
+    },
+    {
+        id: 39, 
+        state: 'Rhode Island', 
+        capital: 'Providence'
+    },
+    {
+        id: 40, 
+        state: 'South Carolina', 
+        capital: 'Columbia'
+    },
+    {
+        id: 41, 
+        state: 'South Dakota', 
+        capital: 'Pierre'
+    },
+    {
+        id: 42, 
+        state: 'Tennessee', 
+        capital: 'Nashville'
+    },
+    {
+        id: 43, 
+        state: 'Texas', 
+        capital: 'Austin'
+    },
+    {
+        id: 44, 
+        state: 'Utah', 
+        capital: 'Salt Lake City'
+    },
+    {
+        id: 45, 
+        state: 'Vermont', 
+        capital: 'Montpelier'
+    },
+    {
+        id: 46, 
+        state: 'Virginia', 
+        capital: 'Richmond'
+    },
+    {
+        id: 47, 
+        state: 'Washington', 
+        capital: 'Olympia'
+    },
+    {
+        id: 48, 
+        state: 'West Virginia', 
+        capital: 'Charleston'
+    },
+    {
+        id: 49, 
+        state: 'Wisconsin', 
+        capital: 'Madison'
+    },
+    {
+        id: 50, 
+        state: 'Wyoming', 
+        capital: 'Cheyenne'
+    },
+
+];
